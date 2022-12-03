ffcb4800c0d0792206e73ac8b9e3144ac09a8087
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-12-03 18:40:38 +0200

    feat: material ui packages installed and setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,32 @@
 import type { AppProps } from "next/app"
+import { CacheProvider, EmotionCache } from "@emotion/react"
+import { ThemeProvider } from "@mui/material/styles"
+import { CssBaseline } from "@mui/material"
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+import { createEmotionCache, theme } from "@/theme"
+
+/**
+ * @description  Client-side cache, shared for the whole session of the user in the browser.
+ */
+const clientSideEmotionCache = createEmotionCache()
+
+type PageProps = {
+  emotionCache?: EmotionCache
+} & AppProps
+
+const App = ({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: PageProps) => {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default App
